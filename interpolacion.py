96dd3af7f175446c2993dcdd482c5d30e8e07c85
David Álvarez Rosa <david@alvarezrosa.com>
2019-03-26 13:18:26 +0100

    Modificado último.

--- interpolacion.py
+++ interpolacion.py
@@ -1,18 +1,18 @@
- # from adafruit_servokit import ServoKit
+from adafruit_servokit import ServoKit
 from time import sleep
 import posicion
 
 from math import pi
 
 
-# kit = ServoKit(channels = 16)
+kit = ServoKit(channels = 16)
 
 
 def move(servo, angle):
     steps = 50
     time = 4
     timeStep = time/steps
-    angleIni
+    angleIni = 90
     h = (angle - angleIni)/50
     for i in range(0, 50):
         angleIni = angleIni + h
@@ -39,3 +39,8 @@ while (True):
     phi4 = convertRadiansToDegrees(phi4)
 
     print(phi1, phi2, phi3, phi4)
+
+    kit.servo[].angle = phi1
+    kit.servo[].angle = phi2
+    kit.servo[].angle = phi3
+    kit.servo[].angle = phi4
