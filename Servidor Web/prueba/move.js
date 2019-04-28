c3e13bc425f23d923a3330b7a098265fc778a915
Guillermo <you@example.com>
2019-04-28 10:24:11 +0200

    Estetica brazo

--- Servidor Web/prueba/move.js
+++ Servidor Web/prueba/move.js
@@ -14,7 +14,10 @@ function myMove() {
             var x = length*Math.cos(deg*Math.PI/180) - length;
             brazo2.style.transform = "translate(" + x.toString() + "px, " + y.toString() + "px)";
             pinza.style.transform = "translate(" + x.toString() + "px, " + y.toString() + "px)";
-        }
+	    tenazas_v1.style.transform = "translate(" + x.toString() + "px, " + y.toString() + "px)";
+	    tenazas_v2.style.transform = "translate(" + x.toString() + "px, " + y.toString() + "px)";
+	    tenazas_h.style.transform = "translate(" + x.toString() + "px, " + y.toString() + "px)";
+	}
     }
 
     var deg2 = 0;
@@ -31,6 +34,40 @@ function myMove() {
             var xp = x + length*(Math.cos(deg2*Math.PI/180) - 1) - 10;
             var yp = y + length*Math.sin(deg2*Math.PI/180);
             pinza.style.transform = "translate(" + xp.toString() + "px, " + yp.toString() + "px)";
+	    tenazas_v1.style.transform = "translate(" + xp.toString() + "px, " + yp.toString() + "px)";
+	    tenazas_v2.style.transform = "translate(" + xp.toString() + "px, " + yp.toString() + "px)";
+	    tenazas_h.style.transform = "translate(" + xp.toString() + "px, " + yp.toString() + "px)";
         }
+    }
+}
+
+function rad_to_deg(phi) {
+    return phi*(180/Math.PI);
+}
+
+function move_brazo(phi1,phi4) {
+    /* Supongo que se pasa phi1 y phi4 en radianes */
+    /* Supongo phi4 < 2*pi */
+
+    var deg = Math.ceil(360-rad_to_deg(phi4));
+    console.log(deg)
+    var id_brazo2 = setInterval(frame4, 500);
+    var length = 250;
+    function frame4() {
+	if (deg == 0) {
+	    clearInterval(id_brazo2)
+	}
+	else {
+	    --deg;
+	    var y = length*Math.sin(deg*Math.PI/180);
+            var x = length*Math.cos(deg*Math.PI/180) - length;
+            brazo2.style.transform = "translate(" + x.toString() + "px, " + y.toString() + "px) rotate(" + deg.toString() + "deg)";
+            var xp = x + length*(Math.cos(deg*Math.PI/180) - 1) - 10;
+            var yp = y + length*Math.sin(deg*Math.PI/180);
+            pinza.style.transform = "translate(" + xp.toString() + "px, " + yp.toString() + "px)";
+	    tenazas_v1.style.transform = "translate(" + xp.toString() + "px, " + yp.toString() + "px)";
+	    tenazas_v2.style.transform = "translate(" + xp.toString() + "px, " + yp.toString() + "px)";
+	    tenazas_h.style.transform = "translate(" + xp.toString() + "px, " + yp.toString() + "px)";
+	}
     }
 }
