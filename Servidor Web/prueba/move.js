46ffaa6f2f664d28880ce3db17b25614acdd047f
Guillermo <you@example.com>
2019-04-28 14:45:42 +0200

    funcion movimiento

--- Servidor Web/prueba/move.js
+++ Servidor Web/prueba/move.js
@@ -1,70 +1,64 @@
-function myMove() {
-    var deg = 0;
-    var id = setInterval(frame, 20);
+function rad_to_deg(phi) {
+    return phi*(180/Math.PI);
+}
+
+
+function move_brazo(phi1,phi2) {
+    /* Supongo que se pasa phi1 y phi2 en grados */
+    /* Supongo phi2 > 0 */
+    /* phi2 --> angulo que forma la barra 2 con la horizontal */
+    
+    var cont1 = 0;
+    var deg1 = Math.ceil(phi1);
+    console.log(deg1)
+    var id = setInterval(frame, 50);
     var length = 250;
     function frame() {
-        if (deg == -60) {
-            clearInterval(id);
-        }
-        else {
-            --deg;
-            brazo.style.transform = "rotate(" + deg.toString() + "deg)";
-            console.log(deg)
-            var y = length*Math.sin(deg*Math.PI/180);
-            var x = length*Math.cos(deg*Math.PI/180) - length;
+        if (cont1 == deg1) {
+	    clearInterval(id);
+	    var y = -length*Math.sin(deg1*Math.PI/180);
+	    var x = length*Math.cos(deg1*Math.PI/180) - length;
+	    move_brazo2(phi1,phi2,x,y);
+	}
+	else {
+	    ++cont1;
+            brazo.style.transform = "rotate(" + -cont1.toString() + "deg)";
+            console.log(cont1)
+            var y = -length*Math.sin(cont1*Math.PI/180);
+            var x = length*Math.cos(cont1*Math.PI/180) - length;
             brazo2.style.transform = "translate(" + x.toString() + "px, " + y.toString() + "px)";
             pinza.style.transform = "translate(" + x.toString() + "px, " + y.toString() + "px)";
 	    tenazas_v1.style.transform = "translate(" + x.toString() + "px, " + y.toString() + "px)";
 	    tenazas_v2.style.transform = "translate(" + x.toString() + "px, " + y.toString() + "px)";
 	    tenazas_h.style.transform = "translate(" + x.toString() + "px, " + y.toString() + "px)";
+	    console.log("out2.0")
 	}
     }
-
-    var deg2 = 0;
-    var id2 = setInterval(frame2, 15);
-    function frame2() {
-        if (deg2 == 30) {
-            clearInterval(id2);
-        }
-        else if (deg == -60) {
-            ++deg2;
-            var y = length*Math.sin(deg*Math.PI/180);
-            var x = length*Math.cos(deg*Math.PI/180) - length;
-            brazo2.style.transform = "translate(" + x.toString() + "px, " + y.toString() + "px) rotate(" + deg2.toString() + "deg)";
-            var xp = x + length*(Math.cos(deg2*Math.PI/180) - 1) - 10;
-            var yp = y + length*Math.sin(deg2*Math.PI/180);
-            pinza.style.transform = "translate(" + xp.toString() + "px, " + yp.toString() + "px)";
-	    tenazas_v1.style.transform = "translate(" + xp.toString() + "px, " + yp.toString() + "px)";
-	    tenazas_v2.style.transform = "translate(" + xp.toString() + "px, " + yp.toString() + "px)";
-	    tenazas_h.style.transform = "translate(" + xp.toString() + "px, " + yp.toString() + "px)";
-        }
-    }
 }
 
-function rad_to_deg(phi) {
-    return phi*(180/Math.PI);
-}
-
-function move_brazo(phi1,phi4) {
-    /* Supongo que se pasa phi1 y phi4 en radianes */
-    /* Supongo phi4 < 2*pi */
-
-    var deg = Math.ceil(360-rad_to_deg(phi4));
-    console.log(deg)
-    var id_brazo2 = setInterval(frame4, 500);
+function move_brazo2(phi1,phi2,x,y) {
+    console.log(x)
+    console.log(y)
+    var deg1 = Math.ceil(phi1);
+    var deg2 = Math.ceil(phi2);
     var length = 250;
-    function frame4() {
-	if (deg == 0) {
-	    clearInterval(id_brazo2)
+    console.log(deg2)
+    var cont2 = 0;
+    var id2 = setInterval(frame2, 50);
+    function frame2() {
+	if (cont2 == deg2) {
+	    clearInterval(id2);
 	}
 	else {
-	    --deg;
-	    var y = length*Math.sin(deg*Math.PI/180);
-            var x = length*Math.cos(deg*Math.PI/180) - length;
-            brazo2.style.transform = "translate(" + x.toString() + "px, " + y.toString() + "px) rotate(" + deg.toString() + "deg)";
-            var xp = x + length*(Math.cos(deg*Math.PI/180) - 1) - 10;
-            var yp = y + length*Math.sin(deg*Math.PI/180);
-            pinza.style.transform = "translate(" + xp.toString() + "px, " + yp.toString() + "px)";
+	    ++cont2;
+
+	    console.log(x);
+	    console.log(cont2);
+	    brazo2.style.transform = "translate(" + x.toString() + "px, " + y.toString() + "px) rotate(" + cont2.toString() + "deg)";
+	    var xp = x + length*(Math.cos(cont2*Math.PI/180) - 1) - 10;
+	    var yp = y + length*Math.sin(cont2*Math.PI/180);
+	    console.log(xp);
+	    pinza.style.transform = "translate(" + xp.toString() + "px, " + yp.toString() + "px)";
 	    tenazas_v1.style.transform = "translate(" + xp.toString() + "px, " + yp.toString() + "px)";
 	    tenazas_v2.style.transform = "translate(" + xp.toString() + "px, " + yp.toString() + "px)";
 	    tenazas_h.style.transform = "translate(" + xp.toString() + "px, " + yp.toString() + "px)";
